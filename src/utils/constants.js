e35fd02548029b478c2b39027fabffcd94b1f048
maryamcheema <22100276@lums.edu.pk>
2024-08-04 23:13:51 -0700

    Adjusted the Vidscribe desc

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,8 +16,8 @@ export const people = [
   {image:"https://media.licdn.com/dms/image/C5603AQHE8rNO86axFw/profile-displayphoto-shrink_200_200/0/1663634479068?e=1727913600&v=beta&t=qA_QhDtGP6B1vUljHLSLhhehJkIPXbYYEvLG6yEjpxY", name: "Maryam S Cheema", info:"Arizona State University", zoom: false},
   {image:"https://media.licdn.com/dms/image/D4D03AQHfxJERdOpCqw/profile-displayphoto-shrink_800_800/0/1720546929013?e=1727913600&v=beta&t=y4C3czpvBgqllkQFyBcUrZa_w-7TSWrhXEkKPKdyKeQ" , name: "Sina Elahimanesh", info:"Arizona State University", zoom: true},
   {image:"https://scholar.googleusercontent.com/citations?view_op=medium_photo&user=IAD0fQQAAAAJ&citpid=6" , name: "Hasti Seifi", info: "Arizona State University", email: "hasti.seifi@asu.edu", site:"https://hastiseifi.com/", zoom: false},
-  {image:"https://webapp4.asu.edu/photo-ws/directory_photo/pfazli?size=medium&break=1715630896" , name: "Pooyan Fazli", info: "Arizona State University.", email:"pooyan@asu.edu", site:"https://pooyanfazli.com/", zoom: false},
+  {image:"https://pooyanfazli.com/images/PooyanFazli.jpg" , name: "Pooyan Fazli", info: "Arizona State University.", email:"pooyan@asu.edu", site:"https://pooyanfazli.com/", zoom: false},
 ]
 
-export const aboutWebsite = ["Welcome to VidScribe, a platform designed specficically to empower blind and low vision users to create AI audio descriptions for videos. On Vidscribe, you can upload videos or paste a link of a youtube video to be described. After a while, a described video will be visible on the homepage. The audio descriptions are generated using a vision language model.",
-"By providing a platform where blind users can independently create audio descriptions, our aim with VidScribe is inclusivity and providing equal access to information and entertainment."]
+export const aboutWebsite = ["Welcome to VidScribe, a platform designed to empower blind and low vision users by providing AI-generated audio descriptions for videos and allowing users to ask questions about the content.",
+"On VidScribe, you can upload videos or paste a YouTube link to receive descriptions. Once processed, the described video will appear on your homepage. Our goal with VidScribe is to foster inclusivity and ensure equal access to information for blind and low vision users."]
